2110f4bd8984e19ff74ce6a83c6577834d57137a
Nathan Geronimo <nathangero@yahoo.com>
2025-01-06 14:09:46 -0800

    show logged in user data in Header

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,9 +1,19 @@
+"use client'"
+
+import { type Session } from "next-auth";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-export const Header = () => {
-  const { data: sessionData } = useSession();
+export const Header = ({ sessionData }: HeaderProps) => {
 
   return (
-    
+    <div className="navbar bg-primary text-primary-content">
+      <div className="flex-1 pl-5 text-3xl font-bold">
+        {sessionData?.user?.name ? `Notes for ${sessionData.user.name}` : ""}
+      </div>
+    </div>
   )
+}
+
+interface HeaderProps {
+  sessionData: Session | null;
 }
