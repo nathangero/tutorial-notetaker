85c8545ef2125aee71102dc2526c1437d0d99e4e
Nathan Geronimo <nathangero@yahoo.com>
2025-01-07 10:21:31 -0800

    implement adding a new topic and showing it

--- src/app/_components/Content.tsx
+++ src/app/_components/Content.tsx
@@ -5,9 +5,42 @@ import { Session } from "next-auth";
 
 export const Content = ({ sessionData }: ContentProps) => {
   const [topics] = api.topic.getAll.useSuspenseQuery();
+  const createTopic = api.topic.create.useMutation({});
 
-
-  return <div>{JSON.stringify(topics)}</div>
+  return (
+    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <div className="px-2">
+        <ul className="menu rounded-box w-56 bg-base-100 p-2">
+          {topics?.map((topic) => (
+            <li key={topic.id}>
+              <a
+                href="#"
+                onClick={(evt) => {
+                  evt.preventDefault();
+                }}
+              >{topic.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="divider" />
+        <input
+          type="text"
+          placeholder="New Topic"
+          className="input-bordered input input-sm w-full"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              createTopic.mutate({
+                title: e.currentTarget.value,
+              });
+              e.currentTarget.value = "";
+            }
+          }}
+        />
+      </div>
+      <div className="col-span-3"></div>
+    </div>
+  )
 }
 
 interface ContentProps {
